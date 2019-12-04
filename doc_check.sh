adfc4cde6a6f30698ec0ca1f0e4821669ed9f7e4
Gib-Sarpy <gfilter@sarpy.com>
2019-12-04 15:17:42 -0800

    fixes p.1

--- doc_check.sh
+++ doc_check.sh
@@ -3,7 +3,10 @@ if [ -z "$1" ]
   then
     echo "No argument supplied, please put in document name"
 fi
+
 name=$1
+echo $name
 doc2pdf $name
-name=${name::4}
-pdftoppm $name.pdf ${name} -png
+name=${name}.pdf
+echo $name
+pdftoppm $name ${name::4} -png
